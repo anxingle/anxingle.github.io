ac700a1be1ddc1ea448d0420291a970dadffdfb4
anxingle <anxingle@163.com>
2015-10-30 09:11:53 +0800

    百度分析

---
+++ public/js/baidu.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?d34d797a3cb2f610267013cfe7d8daeb";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
