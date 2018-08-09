13a7454a2e81b57848398bd59cf7148a67f5edc1
安兴乐 <anxingle@shensz.cn>
2018-08-09 17:16:43 +0800

    Test： 数学公式

--- public/js/math.js
+++ public/js/math.js
@@ -1,6 +1,10 @@
+<script type="text/javascript" async
+  src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
 
-$(document).ready(function() {
-    GenerateContentList();
-    // 测试
-    loadScript("https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML");
-});
+
+// $(document).ready(function() {
+//     GenerateContentList();
+//     // 测试
+//     loadScript("https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML");
+// });
